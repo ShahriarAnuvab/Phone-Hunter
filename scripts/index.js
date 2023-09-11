d4d681889f1779ee3a6589ee190c4494599ebfdb
ShahriarAnuvab <shahriarrahmananuvab@gmail.com>
2023-09-12 04:38:22 +0600

    done

--- scripts/index.js
+++ scripts/index.js
@@ -7,7 +7,7 @@ const loadData = async (searchText, isShowAll) => {
   // console.log(phones)
   displayPhones(phones, isShowAll);
 };
-// loadData();
+// loadData('13');
 const displayPhones = (phones, isShowAll) => {
   const cardContainer = document.getElementById("cardContainer");
   cardContainer.textContent = "";
@@ -39,7 +39,7 @@ const displayPhones = (phones, isShowAll) => {
       <p class="text-[#706F6F] text-lg font-normal">There are many variations of passages of available, but the majority have suffered</p>
       <p class="text-[#100F0F] text-xl font-semibold">$999</p>
       <div class="card-actions">
-        <button class="btn bg-[#0D6EFD] text-white rounded-lg ">Show Details</button>
+        <button class="btn bg-[#0D6EFD] text-white rounded-lg " onclick="showDetails('${phone.slug}')">Show Details</button>
       </div>
     </div>
   </div>`;
@@ -69,3 +69,43 @@ const toggleHandler = (isLoading) => {
 const showAll = () => {
   handleSearch(true);
 };
+
+const showDetails = async (id)=>{
+  // console.log('Show Details', id)
+  const res = await fetch(`https://openapi.programming-hero.com/api/phone/${id}`)
+  const data = await res.json();
+  // console.log(data)
+  showModal(data);
+};
+const showModal = (modalDetails)=>{
+  modal_Details.showModal();
+  console.log(modalDetails.data)
+  const phoneDetails = document.getElementById('phoneDetails');
+  phoneDetails.textContent = '';
+  const div = document.createElement('div');
+  div.innerHTML= `
+  <div  class=" grid justify-items-center">
+  <img src=${modalDetails.data.image} alt="">
+</div>
+
+<h3 class="font-bold text-lg" id="modalPhoneName">${modalDetails.data.brand}</h3>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal">It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Storage :</span> ${modalDetails.data?.mainFeatures?.storage}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Display Size : </span>  ${modalDetails.data?.mainFeatures?.displaySize}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Chipset :</span> ${modalDetails.data?.mainFeatures?.chipSet}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Memory :</span>  ${modalDetails.data?.mainFeatures?.memory}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Slug :</span>  ${modalDetails.data?.slug}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Release data :</span> ${modalDetails.data?.releaseDate}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">Brand :</span> ${modalDetails.data?.brand}</p>
+<p class="text-[#706F6F] text-xs md:text-sm lg:text-lg font-normal"><span class="font-medium text-black">GPS :</span> ${modalDetails.data?.others?.GPS}</p>
+
+`    
+phoneDetails.appendChild(div)           
+
+}
+
+
+
+
+
+
